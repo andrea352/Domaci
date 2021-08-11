e8bd657178dc4da1c13da9c54ebb87b5ab229334
andrea352 <88157929+andrea352@users.noreply.github.com>
2021-08-11 15:52:45 +0200

    Create 1.js

---
+++ nedelja3/sreda/1.js
@@ -0,0 +1,71 @@
+//1.
+{
+    let zbir = 0
+    let proizvod = 1
+    let niz3 = []
+    function sumImnozenje(niz) {
+        for (let i = 0; i < niz.length; i++) {
+            zbir += niz[i]
+
+            proizvod *= niz[i]
+        }
+        niz3.push(zbir)
+        niz3.push(proizvod)
+        return niz3
+    }
+
+
+    let niz2 = sumImnozenje([1, 2, 3, 4, 5])
+    console.log(niz2);
+
+    console.log('---------');
+}
+//2.
+{
+
+    function izbacivanjeEl(niz) {
+        let niz1 = []
+        for (let i = 0; i < niz.length; i++) {
+            if (niz[i]) {
+                niz1.push(niz[i])
+            }
+        }
+        return niz1
+
+    }
+    console.log(izbacivanjeEl([1, 2, undefined, 89, null, 'string', NaN, '']))
+}
+
+console.log('---------');
+//4.
+{
+    let x = 10
+    let y = 5
+    x = x + y
+    y = x - y
+    x = x - y
+    console.log(`x=${x},y=${y}`);
+}
+
+console.log('----------');
+//3.
+{
+    let N = 100
+    for (let i = 0; i < N; i++) {
+        if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+            console.log(`FizzBuzzZazz`);
+        } else if (i % 3 == 0 && i % 5 == 0) {
+            console.log(`FizzBuzz`);
+        } else if (i % 3 == 0 && i % 7 == 0) {
+            console.log(`FizzZazz`);
+        } else if (i % 5 == 0 && i % 7 == 0) {
+            console.log(`BuzzZazz`);
+        } else if (i % 3 == 0) {
+            console.log(`Fizz`);
+        } else if (i % 5 == 0) {
+            console.log(`Buzz`);
+        }
+    }
+}
+
+
